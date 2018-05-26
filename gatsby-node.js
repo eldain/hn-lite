1b5728b082fb437399afad67a079bf2c4049b05a
Daniel McInnis <dmcinnis@uci.edu>
2018-05-25 22:02:00 -0700

    Fix async/await error on build

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,4 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
- // You can delete this file if you're not using it
+exports.modifyBabelrc = ({ babelrc }) => ({
+  ...babelrc,
+  plugins: babelrc.plugins.concat(['transform-regenerator']),
+})
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,37 +6,37 @@ injectGlobal`
   body{
     background-color: #141414;
   }
-`;
+`
 
 const Title = styled.h1`
   font-size: 1.5rem;
   color: #f3e8ee;
-`;
+`
 
 const List = styled.ul`
   padding-left: 0;
   list-style-type: none;
-`;
+`
 
 class App extends React.Component {
   state = {
-    topStories: []
-  };
+    topStories: [],
+  }
   setStateAsync(state) {
     return new Promise(resolve => {
-      this.setState(state, resolve);
-    });
+      this.setState(state, resolve)
+    })
   }
   async componentDidMount() {
     const res = await fetch(
-      "https://hacker-news.firebaseio.com/v0/topstories.json"
-    );
-    const topStories = await res.json();
+      'https://hacker-news.firebaseio.com/v0/topstories.json'
+    )
+    const topStories = await res.json()
     const topStoryRes = await fetch(
-      "https://hacker-news.firebaseio.com/v0/item/17075489.json"
-    );
-    const topStory = await topStoryRes.json();
-    await this.setStateAsync({ topStories });
+      'https://hacker-news.firebaseio.com/v0/item/17075489.json'
+    )
+    const topStory = await topStoryRes.json()
+    await this.setStateAsync({ topStories })
   }
 
   render() {
@@ -49,7 +49,7 @@ class App extends React.Component {
             .map((story, i) => <Post id={story} key={i} />)}
         </List>
       </React.Fragment>
-    );
+    )
   }
 }
 
